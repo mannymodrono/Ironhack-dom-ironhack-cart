4828aea0d4e108371eceb87f9ba9e9103d6aa4e7
Manny Modrono <21243@columbushs.com>
2019-12-09 11:15:36 -0500

    changed update

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -2,14 +2,45 @@ var $cart = document.querySelector('#cart tbody');
 var $calc = document.getElementById('calc');
 
 function updateSubtot($product) {
-  let quantity = document.getElementById("qt").value;
-  let price = document.getElementById('price').innerHTML;
-  let subtotal = quantity * price;
-  let printedSub = document.getElementById('sub').innerHTML = subtotal;
-  return printedSub;
+  // let quantity = document.getElementById("qt").value;
+  // let price = document.getElementById('price').innerHTML;
+  // let subtotal = quantity * price;
+  // let printedSub = document.getElementById('sub').innerHTML = subtotal;
+  // return printedSub;
+
+  let products = [...document.getElementsByClassName('product')];
+
+  products.forEach(product => {
+    let qty = product.children[2].children[0].children[0].value;
+    console.log(qty)
+    let price = product.children[1].children[0].innerHTML;
+    console.log(price)
+    let sub = product.children[3].children[0].innerHTML;
+    console.log(sub)
+
+    sub = qty * price;
+    product.children[3].children[0].innerHTML = sub;
+  });
 }
 
 function calcAll() {
-  document.getElementById('total').innerHTML = updateSubtot();
+  
+  // let products = [...document.getElementsByClassName('product')];
+  // let total = 0;
+
+  // products.forEach(product => {
+  //   let qty = product.children[2].children[0].children[0].value;
+  //   console.log(qty)
+  //   let price = product.children[1].children[0].innerHTML;
+  //   console.log(price)
+  //   let sub = product.children[3].children[0].innerHTML;
+  //   console.log(sub)
+
+  //   sub = qty * price;
+  //   total += sub;
+  // });
+
+  // document.getElementById('total').innerHTML = total;
 }
-$calc.onclick = calcAll;
+
+$calc.onclick = calcAll();
